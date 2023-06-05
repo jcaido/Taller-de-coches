5524753dabfb8a00f0971e272a3a73357a500245
Cristobal Rosa <crisrogar@gmail.com>
2023-06-05 12:29:43 +0200

    test: test para guardar un codigo postal

--- src/test/java/com/Tallerdecoches/repositories/CodigoPostalRepositoryTest.java
+++ src/test/java/com/Tallerdecoches/repositories/CodigoPostalRepositoryTest.java
@@ -1,9 +1,46 @@
 package com.Tallerdecoches.repositories;
 
+import com.Tallerdecoches.entities.CodigoPostal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class CodigoPostalRepositoryTest {
+
+    @Autowired
+    private CodigoPostalRepository codigoPostalRepository;
+
+    private CodigoPostal codigoPostal;
+
+    @BeforeEach
+    void setUp() {
+        codigoPostal = CodigoPostal.builder()
+                .codigo("14500")
+                .localidad("Lucena")
+                .provincia("Cordoba")
+                .build();
+        codigoPostalRepository.save(codigoPostal);
+    }
+
+    @DisplayName("Test para guardar un codigo postal")
+    @Test
+    void guardarCodigoPostalTest() {
+        CodigoPostal codigoPostal1 = CodigoPostal.builder()
+                .codigo("14920")
+                .localidad("Aguilar de la Frontera")
+                .provincia("Cordoba")
+                .build();
+        CodigoPostal codigoPostalGuardado = codigoPostalRepository.save(codigoPostal1);
+        assertThat(codigoPostalGuardado).isNotNull();
+        assertThat(codigoPostalGuardado.getId()).isGreaterThan(0);
+        assertThat(codigoPostalGuardado.getCodigo()).isEqualTo("14920");
+    }
+
 }
