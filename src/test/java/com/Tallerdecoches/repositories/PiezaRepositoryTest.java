30425053235a2d97176d0d1d8ac04e6c588e09a4
Cristobal Rosa <crisrogar@gmail.com>
2023-06-13 14:03:21 +0200

    test: test para guardar una pieza con la misma referencia

--- src/test/java/com/Tallerdecoches/repositories/PiezaRepositoryTest.java
+++ src/test/java/com/Tallerdecoches/repositories/PiezaRepositoryTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
@@ -40,5 +42,18 @@ public class PiezaRepositoryTest {
         Pieza piezaGuardada = piezaRepository.save(pieza1);
         assertThat(piezaGuardada.getId()).isGreaterThan(0);
         assertThat(piezaGuardada.getReferencia()).isEqualTo("BBBB");
+    }
+
+    @DisplayName("Test para guardar una pieza con la misma referencia")
+    @Test
+    void guarPiezaMismaReferenciaTest() {
+        Pieza pieza1 = Pieza.builder()
+                .referencia("AAAA")
+                .nombre("tornillo")
+                .precio(0.65)
+                .build();
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            piezaRepository.save(pieza1);
+        });
     }
 }
