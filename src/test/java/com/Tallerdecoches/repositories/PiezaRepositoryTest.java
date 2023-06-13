89390ac539d741820ce12e5ae95f1d51b6aa739d
Cristobal Rosa <crisrogar@gmail.com>
2023-06-13 14:01:57 +0200

    test: test para guardar una pieza

--- src/test/java/com/Tallerdecoches/repositories/PiezaRepositoryTest.java
+++ src/test/java/com/Tallerdecoches/repositories/PiezaRepositoryTest.java
@@ -2,9 +2,13 @@ package com.Tallerdecoches.repositories;
 
 import com.Tallerdecoches.entities.Pieza;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
@@ -23,5 +27,18 @@ public class PiezaRepositoryTest {
                 .precio(4.5)
                 .build();
         piezaRepository.save(pieza);
+    }
+
+    @DisplayName("Test para guardar una pieza")
+    @Test
+    void guardarPiezaTest() {
+        Pieza pieza1 = Pieza.builder()
+                .referencia("BBBB")
+                .nombre("tornillo")
+                .precio(0.65)
+                .build();
+        Pieza piezaGuardada = piezaRepository.save(pieza1);
+        assertThat(piezaGuardada.getId()).isGreaterThan(0);
+        assertThat(piezaGuardada.getReferencia()).isEqualTo("BBBB");
     }
 }
