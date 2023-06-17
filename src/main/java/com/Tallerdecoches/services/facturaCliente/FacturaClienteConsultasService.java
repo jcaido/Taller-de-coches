f4274a413c867e4a7447550791f653fb72c8a726
Cristobal Rosa <crisrogar@gmail.com>
2023-06-17 18:56:51 +0200

    refact: asignar numero de factur crear factura

--- src/main/java/com/Tallerdecoches/services/facturaCliente/FacturaClienteConsultasService.java
+++ src/main/java/com/Tallerdecoches/services/facturaCliente/FacturaClienteConsultasService.java
@@ -2,6 +2,7 @@ package com.Tallerdecoches.services.facturaCliente;
 
 import com.Tallerdecoches.DTOs.facturaCliente.FacturaClienteCrearDTO;
 import com.Tallerdecoches.entities.FacturaCliente;
+import com.Tallerdecoches.repositories.FacturaClienteRepository;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -13,9 +14,11 @@ import java.util.List;
 public class FacturaClienteConsultasService {
 
     private final EntityManager entityManager;
+    private final FacturaClienteRepository facturaClienteRepository;
 
-    public FacturaClienteConsultasService(EntityManager entityManager) {
+    public FacturaClienteConsultasService(EntityManager entityManager, FacturaClienteRepository facturaClienteRepository) {
         this.entityManager = entityManager;
+        this.facturaClienteRepository = facturaClienteRepository;
     }
 
     public List<FacturaCliente> obtenerFacturasClientesEntreFechas(FacturaClienteCrearDTO facturaDTO) {
@@ -42,4 +45,31 @@ public class FacturaClienteConsultasService {
 
         return factura;
     }
+
+    public void asignarNumeroFacturaCrearFactura(FacturaCliente facturaCliente, FacturaClienteCrearDTO facturaClienteCrearDTO) {
+        if (obtenerFacturasClientesEntreFechas(facturaClienteCrearDTO).isEmpty()) {
+            facturaClienteRepository.save(facturaCliente);
+            facturaCliente.setNumeroFactura(1L);
+            facturaClienteRepository.save(facturaCliente);
+        } else {
+            facturaClienteRepository.save(facturaCliente);
+            facturaCliente.setNumeroFactura(obtenerFacturaMaximoNumeroFacturaEntreFechas(facturaClienteCrearDTO).get(0).getNumeroFactura() + 1);
+            facturaClienteRepository.save(facturaCliente);
+        }
+    }
+
+    public List<FacturaCliente> ObtenerUltimaFacturaEntreFechas(LocalDate fechaIncial, LocalDate fechaFinal) {
+        Query query = entityManager.createNativeQuery("SELECT * FROM facturas_clientes " +
+                "WHERE fecha_factura >= :fechaInicial " +
+                "AND fecha_factura <= :fechaFinal " +
+                "AND numero_factura = (SELECT MAX(numero_factura) FROM facturas_clientes WHERE " +
+                " fecha_factura >= :fechaInicial AND fecha_factura <= :fechaFinal)", FacturaCliente.class);
+        query.setParameter("fechaInicial", fechaIncial);
+        query.setParameter("fechaFinal", fechaFinal);
+        List<FacturaCliente> factura = query.getResultList();
+
+        return factura;
+    }
+
+
 }
