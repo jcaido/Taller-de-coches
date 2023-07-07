68b47c073cfb1ba241c4a8f3f02eb9e718f73734
Cristobal Rosa <crisrogar@gmail.com>
2023-07-07 17:38:46 +0200

    feat: documentacion swagger para "crear nueva información de mano de obra"

--- src/main/java/com/Tallerdecoches/controllers/ManoDeObraController.java
+++ src/main/java/com/Tallerdecoches/controllers/ManoDeObraController.java
@@ -6,7 +6,12 @@ import com.Tallerdecoches.DTOs.manoDeObra.ManoDeObraDTO;
 import com.Tallerdecoches.entities.ManoDeObra;
 import com.Tallerdecoches.repositories.ManoDeObraRepository;
 import com.Tallerdecoches.services.manoDeObra.ManoDeObraService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +32,20 @@ public class ManoDeObraController {
         this.manoDeObraRepository = manoDeObraRepository;
     }
 
-
+    @Operation(summary = "Crear nueva información de mano de obra", description = "Crear nueva información de mano de obra")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "información de mano de obra creada correctamente",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ManoDeObraDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "BAD REQUEST",
+                    content = @Content),
+            @ApiResponse(responseCode = "409",
+                    description = "CONFLICT [\"El precio horario ya existe\"]",
+                    content = @Content),
+    })
     @PostMapping()
     public ResponseEntity<ManoDeObraDTO> crearManoDeObra(@Valid @RequestBody ManoDeObraCrearDTO manoDeObraCrearDTO) {
 
