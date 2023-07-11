a0351729b8f744997c1ef9a3393d8712d0d97784
Cristobal Rosa <crisrogar@gmail.com>
2023-07-11 18:11:32 +0200

    feat: documentación swagger para "obtener una lista parcial ordenada por fecha apertura de órdenes de reparación por estado (abiertas o cerradas)"

--- src/main/java/com/Tallerdecoches/controllers/OrdenReparacionController.java
+++ src/main/java/com/Tallerdecoches/controllers/OrdenReparacionController.java
@@ -176,10 +176,19 @@ public class OrdenReparacionController {
 
         return ordenReparacionService.findByCerradaParcial(cerrada);
     }
-
-    //Obtener ordenes de reparacion por estado (abiertas o cerradas) ordenada for fecha apertura, campos(id, fechaApertura, descripcion, kilometros, matricula)
+    @Operation(summary = "Obtener una lista parcial ordenada por fecha apertura con todas las órdenes de reparación por estado (abiertas o cerradas)",
+            description = "Sólo se mostrarán los campos id, fecha apertura, descripción, kilómetros, matrícula")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Órdenes de reparación obtenidas correctamente",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = OrdenReparacionBusquedasDTO.class))
+                    })
+    })
     @GetMapping("/cerrada-parcial-sort/{cerrada}")
-    public List<OrdenReparacionBusquedasParcialDTO> obtenerOrdenesReparacionPorIsCerradaParcialByFechaAperturaAsc(@PathVariable Boolean cerrada) {
+    public List<OrdenReparacionBusquedasParcialDTO> obtenerOrdenesReparacionPorIsCerradaParcialByFechaAperturaAsc(
+            @Parameter(description = "true = cerradas, false = abiertas", required = true)
+            @PathVariable Boolean cerrada) {
 
         return ordenReparacionService.findByCerradaParcialByFechaAperturaAsc(cerrada);
     }
