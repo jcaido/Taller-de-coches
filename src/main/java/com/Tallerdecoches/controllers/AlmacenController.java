078352268e6dd188c91aebaa43c5f9667512180b
Cristobal Rosa <crisrogar@gmail.com>
2023-07-13 20:18:53 +0200

    feat: documentación swagger para "obtener el inventario de almacén a una fecha determinada"

--- src/main/java/com/Tallerdecoches/controllers/AlmacenController.java
+++ src/main/java/com/Tallerdecoches/controllers/AlmacenController.java
@@ -4,6 +4,7 @@ import com.Tallerdecoches.DTOs.almacen.MovimientoAlmacenDTO;
 import com.Tallerdecoches.DTOs.almacen.MovimientoPiezaDTO;
 import com.Tallerdecoches.services.almacen.InventarioAlmacenService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -39,8 +40,26 @@ public class AlmacenController {
         return inventarioAlmacenService.obtenerInventarioAlmacenFecha(LocalDate.now());
     }
 
+    @Operation(summary = "Obtener el inventario de almacén a una fecha determinada", description = "Obtener el inventario de almacén a una fecha determinada")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inventario de almacén obtenido correctamente",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MovimientoAlmacenDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "BAD REQUEST",
+                    content = @Content),
+            @ApiResponse(responseCode = "409",
+                    description = "CONFLICT Existen piezas imputadas a Ordenes de Reparación abiertas " +
+                            "con fecha igual o anterior a la fecha solicitada. Debe cerrar esas órdenes para poder obtener " +
+                            "el inventario a la fecha solicitada. La fecha de cierre debe ser la del inventario.",
+                    content = @Content),
+    })
     @GetMapping("/inventario/{fecha}")
-    public List<MovimientoAlmacenDTO> obtenerInventarioAlmacenFecha(@PathVariable(name="fecha") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
+    public List<MovimientoAlmacenDTO> obtenerInventarioAlmacenFecha(
+            @Parameter(description = "fecha del inventario", required = true)
+            @PathVariable(name="fecha") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
 
         return inventarioAlmacenService.obtenerInventarioAlmacenFecha(fecha);
     }
